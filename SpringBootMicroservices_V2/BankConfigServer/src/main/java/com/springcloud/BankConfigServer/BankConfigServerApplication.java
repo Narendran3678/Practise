9864329d89982a07af141612726730547373f8df
narendran_babu <narendran_babu@apple.com>
2024-01-02 16:58:12 +0530

    SpringConfigChangeSupply

--- SpringBootMicroservices_V2/BankConfigServer/src/main/java/com/springcloud/BankConfigServer/BankConfigServerApplication.java
+++ SpringBootMicroservices_V2/BankConfigServer/src/main/java/com/springcloud/BankConfigServer/BankConfigServerApplication.java
@@ -19,6 +19,15 @@ public class BankConfigServerApplication {
 	http://localhost:8070/loan-application/default
 	http://localhost:8070/loan-application/uat
 	http://localhost:8070/loan-application/prod
+
+	http://localhost:8070/decrypt
+	http://localhost:8070/encrypt
+
+	http://localhost:8080/bank/actuator/refresh
+	http://localhost:8090/bank/actuator/refresh
+	http://localhost:9001/bank/actuator/refresh
+
+
 	*/
 
 	public static void main(String[] args) {
